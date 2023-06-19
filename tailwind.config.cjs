b061a6a076c49dd98dac242129de680f909f037b
Dylan Paulus <dylan.paulus@rover.com>
2023-06-18 20:02:20 -0700

    basic redesign

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      sans: ["DM Sans"],
+    },
+    extend: {
+      animation: {
+        fadeIn: "fadeIn 1s ease-in-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0, transform: "translateX(-20px)" },
+          "100%": { opacity: 1, transform: "translateX(0)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
