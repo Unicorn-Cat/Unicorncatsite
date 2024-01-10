45e38b076b7f5219b3a01e128007ce88cfff17c2
ganderzz <stolen3@gmail.com>
2024-01-09 21:25:55 -0800

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,20 @@ import tailwind from "@astrojs/tailwind";
 
 const ASSET_PATH = process.env.BASE_URL || "/";
 
-console.log(`----\nASSET_PATH: ${ASSET_PATH}\n${JSON.stringify(import.meta.env)}\n----`);
+console.log(
+  `----\nASSET_PATH: ${JSON.stringify({
+    build: {
+      assets: "_assets",
+      assetsPrefix: ASSET_PATH,
+    },
+  })}\n----`
+);
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   build: {
+    assets: "_assets",
     assetsPrefix: ASSET_PATH,
   },
 });
