7dba8ad80a5091c0ac13f1eddcbe21810734e0c4
ganderzz <stolen3@gmail.com>
2024-01-09 20:30:15 -0800

    fix missing dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import sanity from "astro-sanity";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sanity({
-    projectId: '6o5c8g4q',
-    dataset: 'production',
-    useCdn: true,
-  })]
+  integrations: [tailwind()],
 });
