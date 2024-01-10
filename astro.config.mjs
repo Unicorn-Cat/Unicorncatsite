b5a3069bbfaaba25af9daded727d029c1b46aae1
ganderzz <stolen3@gmail.com>
2024-01-09 21:03:01 -0800

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+const BASE_URL = import.meta.env.BASE_URL || "/";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  build: {
+    assetsPrefix: BASE_URL,
+  },
 });
